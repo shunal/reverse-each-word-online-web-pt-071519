c434b7b69c39626e842a0270c3ed61aa55f567a9
Steven Hunal <stevehunal@gmail.com>
2019-07-26 01:12:38 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+require "pry"
+def reverse_each_word(string)
+  new_array = []
+  array = string.split
+  array.collect do |word|
+    new_array << word.reverse
+  end 
+  new_array.join(" ")
+end 
+
